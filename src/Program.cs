6e1635afabf29eb403a2bc345395984b9ed92bcb
unknown <592640424@qq.com>
2021-09-24 10:05:54 +0800

    change the sln name

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,7 @@ using System.Net;
 using System.Net.Mail;
 using System.Timers;
 
-namespace ConsoleApp
+namespace TeamsNotification
 {
     public class Program
     {
--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("ConsoleApp")]
+[assembly: AssemblyTitle("TeamsNotification")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp")]
+[assembly: AssemblyProduct("TeamsNotification")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
